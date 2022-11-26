88ade41f53471feaf043deaa5efbb3d833fe4ee1
Marcelo Ferreira Vasconcelos <marcelo.fvas@gmail.com>
2022-11-26 20:24:29 -0300

    added log for login poperation

--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -8,8 +8,10 @@ class LoginController {
     authenticate(request: Request, response: Response) {
         try {
             const token = LoginService.login(request.body);
+            console.log(`Login successful`)
             return response.status(HttpStatus.OK).json(token);
         } catch (error) {
+            console.log(`Login Error`)
             return response.status(HttpStatus.UNAUTHORIZED).send();
         }
     }
